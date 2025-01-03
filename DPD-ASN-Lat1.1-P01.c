53452689cfc30896754a68802b40a15ccc1b4468
CodeAhmadSobirin <154493909+CodeAhmadSobirin@users.noreply.github.com>
2025-01-03 18:52:42 +0700

    Add files via upload

---
+++ DPD-ASN-Lat1.1-P01.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int main () {
+	printf ("  \n  ====================================================================================================================    ");
+	printf ("  \n                                                P01 - TUGAS MANDIRI : ASN                                                 ");
+	printf ("  \n  ====================================================================================================================    ");
+	printf ("  \n");
+	printf ("  \n               AAAAAAAAAAAAAAAAAAAAA               SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS      NNNNNNNNN             NNNNNN");
+	printf ("  \n              AAAAAAAAAAAAAAAAAAAAAAA              SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS      NNNNNNNNNN            NNNNNN");
+	printf ("  \n             AAAAAAAAAAAAAAAAAAAAAAAAA             SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS      NNNNNNNNNNN           NNNNNN");
+	printf ("  \n            AAAAAAAAAAAAAAAAAAAAAAAAAAA            SSSSSSSSSS                           NNNNNNNNNNNN          NNNNNN");
+	printf ("  \n           AAAAAAAAA            AAAAAAAA           SSSSSSSSSS                           NNNNNN NNNNNN         NNNNNN");
+	printf ("  \n          AAAAAAAAA              AAAAAAAA          SSSSSSSSSS                           NNNNNN  NNNNNN        NNNNNN");
+	printf ("  \n         AAAAAAAAA                AAAAAAAA         SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS      NNNNNN   NNNNNN       NNNNNN");
+	printf ("  \n        AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA        SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS      NNNNNN    NNNNNN      NNNNNN");
+	printf ("  \n       AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA       SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS      NNNNNN     NNNNNN     NNNNNN");
+	printf ("  \n      AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA                           SSSSSSSSSS      NNNNNN      NNNNNN    NNNNNN");
+	printf ("  \n     AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA                          SSSSSSSSSS      NNNNNN       NNNNNN   NNNNNN");
+	printf ("  \n    AAAAAAAAA                        AAAAAAAAAA                         SSSSSSSSSS      NNNNNN        NNNNNN  NNNNNN");
+	printf ("  \n   AAAAAAAAA                          AAAAAAAAAA   SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS      NNNNNN         NNNNNN NNNNNN");
+	printf ("  \n  AAAAAAAAA                            AAAAAAAAAA  SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS      NNNNNN          NNNNNNNNNNNN");
+	printf ("  \n AAAAAAAAA                              AAAAAAAAAA SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS      NNNNNN           NNNNNNNNNNN");
+	printf ("  \n");
+	printf ("  \n  ===================================================================================================================     ");
+	printf ("  \n");
+	printf ("  \n      NPM    :  432007006220166 ");
+	printf ("  \n      Nama   :  Ahmad Sobirin   ");
+	printf ("  \n      Kelas  :  D               ");
+	printf ("  \n      Matkul :  Dasar Pemograman");
+	printf ("  \n");
+	printf ("  \n  ===================================================================================================================      ");
+	printf ("  \n                                                 STMIK TASIKMALAYA - 2022                                                  ");
+	printf ("  \n  ===================================================================================================================      ");
+	
+	
+	
+		
+}
