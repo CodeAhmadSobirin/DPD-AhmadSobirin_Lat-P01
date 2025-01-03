acfef67189b8750a5f0ccc8a61121aa83b546fb0
CodeAhmadSobirin <154493909+CodeAhmadSobirin@users.noreply.github.com>
2025-01-03 18:51:20 +0700

    Add files via upload

---
+++ DPD-Ahmad Sobirin_Lat1.2-P01.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main () {
+	printf ("\n ");
+	printf ("=====================\n");
+	printf (" Hello World\n");
+	printf (" Program Bahasa C\n");
+	printf (" =====================\n");
+	printf (" Nama : Ahmad Sobirin\n");
+	printf (" Kelas: D\n");
+	printf (" =====================\n");
+	
+	
+	
+}
